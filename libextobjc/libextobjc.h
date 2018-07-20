0c823f8054699b32ff08d7b504f0e504e8bd9259
Mads Bøgeskov <mads@bogeskov.net>
2018-07-20 19:52:04 +0200

    Added framework target

---
+++ libextobjc/libextobjc.h
@@ -0,0 +1,18 @@
+//
+//  libextobjc.h
+//  libextobjc
+//
+//  Created by Mads Bogeskov on 20/07/2018.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for libextobjc.
+FOUNDATION_EXPORT double libextobjcVersionNumber;
+
+//! Project version string for libextobjc.
+FOUNDATION_EXPORT const unsigned char libextobjcVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <libextobjc/PublicHeader.h>
+
+
